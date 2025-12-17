f6a7f5ee4d0f2629c98f3fd5f878c2504d9183ec
lupben <lupben@protonmail.com>
2025-12-17 23:01:41 +0200

    Complete word count task

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("greetings boots")
+
+
+
+def get_book_text(filepath):
+    return open(filepath).read()
+
+
+def main():
+    book_text = get_book_text('/home/warden/workspace/github.com/lupben/bookbot/books/frankenstein.txt')
+    word_count=num_of_words(book_text)
+    print(f"Found {word_count} total words")
+
+def num_of_words(text):
+    words = text.split()
+    return len(words)
+
+
+main()
+
+
+
+
+
+
+
+
