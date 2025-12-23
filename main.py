05876877879df822c69be209af78f8ed84c14936
lupben <lupben@protonmail.com>
2025-12-23 23:27:13 +0200

    learned what sys.argv is

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import num_of_words, count_char, sorted_list_dic
+import sys
 
 
 def get_book_text(filepath):
@@ -6,15 +7,22 @@ def get_book_text(filepath):
 
 
 def main():
-    filepath = "/home/warden/workspace/github.com/lupben/bookbot/books/frankenstein.txt"
-    book_text = get_book_text(filepath)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
+    book_text = get_book_text(sys.argv[1])
 
+
+
+    filepath = sys.argv[1]
     word_count = num_of_words(book_text)
     char_count = count_char(book_text)
     sorted_chars = sorted_list_dic(char_count)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -26,6 +34,7 @@ def main():
         
 
     print("============= END ===============")
+    
 
 
 main()
